98e9db2853063d4df1873d0f35ac899bf1891824
allencustodio <80247893+allenchristiancustodio@users.noreply.github.com>
2022-12-12 15:59:49 +0800

    Update .gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -16,7 +23,14 @@ import { ProductListComponent } from './components/products/product-list/product
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/products/product-list/product-list.component.ts
+++ src/app/components/products/product-list/product-list.component.ts
@@ -1,6 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 import { ProductsService } from 'src/app/services/products.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+
+
+export interface ProductData{
+   prodctid: string;
+   name: string;
+   productNumber: string;
+   listPrice:string;
+   safetyStockLevel: string;
+   color:string;
+   modifiedDate?: Date;
+
+}
 
 @Component({
   selector: 'app-product-list',
@@ -8,22 +23,46 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+ 
   products: any;
+  displayedColumn: string[] = ['productId', 'name', 'productNumber', 'listPrice', 'safetyStockLevel', 'color' ];
+  dataSource!: MatTableDataSource<ProductData>;
+  maxall : number=20;
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort  
+  
+
   constructor( private productService: ProductsService) { }
 
   ngOnInit(): void {
-
+  
     this.productService.getAllProducts().subscribe({
       next:(products) =>{
         this.products = products;
         console.log(products);
+
+        this.dataSource = new MatTableDataSource(this.products)
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+
       },
       error:(response) =>{
         console.log(response);
       }
     })
 
+  
+
   } 
+
+  applyFilter(event: Event){
+    const filterValue = (event?.target as HTMLInputElement).value;
+
+    this.dataSource.filter = filterValue.trim().toLowerCase()
+
+    if (this.dataSource.paginator){
+      this.dataSource.paginator.firstPage()
+    }
+ } 
 
 }
